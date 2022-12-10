61a1bda8b0fbfa19990d4220d1ec374638bc6dc9
jimmym620 <jimmym620@gmail.com>
2022-12-10 22:24:05 +0000

    working get user request

--- room-reviewer/pages/api/user/requests.js
+++ room-reviewer/pages/api/user/requests.js
@@ -3,14 +3,21 @@ import Review from "../../../mongoDB/models/reviewModel";
 
 export default async function handler(req, res) {
     const id = req.query.userId;
+    console.log(id);
     if (req.method === "GET") {
         await connectMongo();
-
-        await Review.find({ authorID: id }, function (err, foundReviews) {
+        await Review.find({ authorID: id }).then(function (err, foundReviews) {
             if (err) {
                 res.send(err);
-            } else res.send(foundReviews);
+            } else {
+                if (foundReviews) {
+                    res.send(foundReviews);
+                } else {
+                    res.status().send("No reviews found for this user");
+                }
+            }
         });
+
         //non GET requests
     } else {
         res.status(401).send("Not authorised");
